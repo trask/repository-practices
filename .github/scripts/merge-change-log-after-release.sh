a82c924eaff2fc8493bf7adf5322da036f0d6ddd
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-11-09 17:12:43 -0800

    Add shellcheck (#112)
    * Add shell check
    
    * Shell check fixes

--- .github/scripts/merge-change-log-after-release.sh
+++ .github/scripts/merge-change-log-after-release.sh
@@ -13,32 +13,31 @@ if [[ $VERSION =~ ^[0-9]+\.[0-9]+\.0 ]]; then
   # the entries are copied over from the release branch to support workflows
   # where change log entries may be updated after preparing the release branch
 
-  # copy the portion above the release, up to and including the heading
-  sed -n "0,/^## Version $VERSION /p" CHANGELOG.md > /tmp/CHANGELOG.md
-
-  # copy the release notes for $VERSION
-  cat /tmp/changelog-section.md >> /tmp/CHANGELOG.md
-
-  # copy the portion below the release
-  sed -n "0,/^## Version $VERSION /d;0,/^## Version /{/^## Version/!d};p" CHANGELOG.md \
-    >> /tmp/CHANGELOG.md
+  {
+    # copy the portion above the release, up to and including the heading
+    sed -n "0,/^## Version $VERSION /p" CHANGELOG.md
+    # copy the release notes for $VERSION
+    cat /tmp/changelog-section.md
+    # copy the portion below the release
+    sed -n "0,/^## Version $VERSION /d;0,/^## Version /{/^## Version/!d};p" CHANGELOG.md
+  } > /tmp/CHANGELOG.md
 
   # update the real CHANGELOG.md
   cp /tmp/CHANGELOG.md CHANGELOG.md
+
 else
   # this was a patch release, so the version does not exist already in the CHANGELOG.md
 
-  # copy the portion above the top-most release, not including the heading
-  sed -n "0,/^## Version /{ /^## Version /!p }" CHANGELOG.md > /tmp/CHANGELOG.md
-
-  # add the heading
-  echo "## Version $VERSION ($RELEASE_DATE)" >> /tmp/CHANGELOG.md
-
-  # copy the release notes for $VERSION
-  cat /tmp/changelog-section.md >> /tmp/CHANGELOG.md
-
-  # copy the portion starting from the top-most release
-  sed -n "/^## Version /,\$p" CHANGELOG.md >> /tmp/CHANGELOG.md
+  {
+    # copy the portion above the top-most release, not including the heading
+    sed -n "0,/^## Version /{ /^## Version /!p }" CHANGELOG.md
+    # add the heading
+    echo "## Version $VERSION ($RELEASE_DATE)"
+    # copy the release notes for $VERSION
+    cat /tmp/changelog-section.md
+    # copy the portion starting from the top-most release
+    sed -n "/^## Version /,\$p" CHANGELOG.md
+  } > /tmp/CHANGELOG.md
 
   # update the real CHANGELOG.md
   cp /tmp/CHANGELOG.md CHANGELOG.md
