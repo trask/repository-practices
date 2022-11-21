10a894fe9d5506bb8e4002e51859517bd6096993
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-11-21 12:31:26 -0800

    Reuse workflows (#124)

--- .github/scripts/get-version.sh
+++ .github/scripts/get-version.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
-# TODO get the current version
+# this script is required by prepare-release-branch.sh and prepare-patch-release.sh
+
+# TODO replace with your logic
 sed -n "s/.*version='\([^']\+\)'/\1/p" setup.py
--- .github/scripts/update-version.sh
+++ .github/scripts/update-version.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
-# TODO update the current version
+# this script is required by prepare-release-branch.sh and prepare-patch-release.sh
+
+# TODO replace with your logic
 sed -Ei "s/version='[^']+'/version='$1'/" setup.py
